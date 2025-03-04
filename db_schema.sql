571f9c80721ac408c664745fba22fd88b5867a20
shevtsov-dmitry <shadowstakeus28@gmail.com>
2025-03-04 11:30:30 +0300

    refactor: update schema by adding mutual polygon_id

--- db_schema.sql
+++ db_schema.sql
@@ -1,13 +1,14 @@
 CREATE TABLE dataset (
-    id SERIAL PRIMARY KEY, -- ID из CSV файла, либо автогенирирующееся
-    period VARCHAR(10),      -- Период (например, 'янв.22', 'май.22')
+    id SERIAL PRIMARY KEY, 
+    polygon_id INTEGER, -- повторяющиеся идентификаторы, группирующие геоданные определённого полигона
+    period VARCHAR(10), -- Период (например, 'янв.22', 'май.22')
     station_name VARCHAR(255), -- Название станции
     surveillance_zone_characteristics TEXT, -- Характеристики зоны наблюдения
     adm_area VARCHAR(255),   -- Административный округ
-    district VARCHAR(255),    -- Район
-    parameter VARCHAR(255),   -- Параметр (например, 'Взвешенные частицы РМ2.5')
-    monthly_average DECIMAL,  -- Среднемесячное значение
+    district VARCHAR(255),   -- Район
+    parameter VARCHAR(255),  -- Параметр (например, 'Взвешенные частицы РМ2.5')
+    monthly_average DECIMAL, -- Среднемесячное значение
     monthly_average_pdkss DECIMAL, -- Среднемесячное значение в долях ПДКсс
-    longitude DECIMAL,        -- Долгота
-    latitude DECIMAL         -- Широта
+    longitude DECIMAL, -- Долгота
+    latitude DECIMAL   -- Широта
 );
