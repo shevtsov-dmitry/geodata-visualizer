d95dc42ea4b2aeca9c1cfeb6af036ea91e6e7a4e
shevtsov-dmitry <shadowstakeus28@gmail.com>
2025-03-04 10:20:19 +0300

    refactor: update database table with new format

--- README.md
+++ README.md
@@ -27,3 +27,9 @@ pip install flask folium pandas sqlalchemy psycopg2-binary geoalchemy2 geopandas
 
 _В случае возникновения ошибок при установке, прочитайте, что вывела команда. Там будет описана проблема и
 дополнительные инструкции по её устранению (а именно, что ещё нужно установить и команда для выполнения)._
+
+## Добавление данных из датасета CSV в БД
+
+``` bash
+\copy dataset(id, period, station_name, surveillance_zone_characteristics, adm_area, district, parameter, monthly_average, monthly_average_pdkss, longitude, latitude) FROM 'dataset.csv' DELIMITER ';' CSV HEADER;
+```
--- db_schema.sql
+++ db_schema.sql
@@ -1,16 +1,13 @@
-CREATE TABLE dataset
-(
-    id                                BIGINT PRIMARY KEY,
-    date                              TIMESTAMP,
-    global_id                         BIGINT,
-    station_name                      VARCHAR(255),
-    latitude                          DOUBLE PRECISION,
-    longitude                         DOUBLE PRECISION,
-    surveillance_zone_characteristics VARCHAR(510),
-    adm_area                          VARCHAR(510),
-    district                          VARCHAR(510),
-    location                          VARCHAR(510),
-    parameter                         VARCHAR(255),
-    monthly_average                   DOUBLE PRECISION,
-    monthly_average_pdkss             DOUBLE PRECISION
+CREATE TABLE dataset (
+    id SERIAL PRIMARY KEY, -- ID из CSV файла, либо автогенирирующееся
+    period VARCHAR(10),      -- Период (например, 'янв.22', 'май.22')
+    station_name VARCHAR(255), -- Название станции
+    surveillance_zone_characteristics TEXT, -- Характеристики зоны наблюдения
+    adm_area VARCHAR(255),   -- Административный округ
+    district VARCHAR(255),    -- Район
+    parameter VARCHAR(255),   -- Параметр (например, 'Взвешенные частицы РМ2.5')
+    monthly_average DECIMAL,  -- Среднемесячное значение
+    monthly_average_pdkss DECIMAL, -- Среднемесячное значение в долях ПДКсс
+    longitude DECIMAL,        -- Долгота
+    latitude DECIMAL         -- Широта
 );
