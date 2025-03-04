7faf25db626d483fb24f7c0a6d4b6dd5e2ac38cb
shevtsov-dmitry <shadowstakeus28@gmail.com>
2025-03-04 13:07:19 +0300

    refactor: split long complex main function to subfunctions

--- main.py
+++ main.py
@@ -30,8 +30,31 @@ def get_data(parameter):
 def create_map():
     """Создает карту с полигонами для всех параметров, используя слои Folium."""
     m = folium.Map(location=[55.75, 37.61], zoom_start=10)  # Центр карты — Москва
+    add_base_maps(m)
+    add_geo_parameters(m)
+    apply_custom_styles(m)
+    # Панель слоев для переключения
+    folium.LayerControl(collapsed=False).add_to(m)
+    return m._repr_html_()
+
+
+def add_base_maps(m):
+    base_maps = [
+        {"name": "CartoDB Positron", "tiles": "CartoDB Positron", "attr": "© CartoDB"},
+        {"name": "Esri World Imagery", "tiles": "Esri.WorldImagery", "attr": "Tiles © Esri"}
+    ]
     
-    # Определяем параметры и цвета
+    for base in base_maps:
+        folium.TileLayer(
+            tiles=base["tiles"],
+            name=base["name"],
+            attr=base["attr"],
+            overlay=False,
+            control=True,
+            show=(base["name"] == "OpenStreetMap") 
+        ).add_to(m)
+
+def add_geo_parameters(m):
     parameters = [
         {"name": "Взвешенные частицы РМ2.5 (суточные измерения)", "color": "blue", "label": "PM2.5"},
         {"name": "Взвешенные частицы РМ10", "color": "green", "label": "PM10"},
@@ -40,8 +63,8 @@ def create_map():
     ]
     
     for param in parameters:
-        # Создаем слой для каждого параметра
-        show = param["name"] == "Взвешенные частицы РМ2.5 (суточные измерения)"  # PM2.5 виден по умолчанию
+        # Создание слоя для каждого параметра
+        show = param["name"] == "Взвешенные частицы РМ2.5 (суточные измерения)" 
         layer = folium.FeatureGroup(name=param["label"], show=show)
         data = get_data(param["name"])
         
@@ -63,12 +86,28 @@ def create_map():
                 fill_color=param["color"]
             ).add_to(layer)
         layer.add_to(m)
-    
-    # Добавляем контроллер слоев для переключения
-    folium.LayerControl().add_to(m)
-    return m._repr_html_()
 
-# Главный маршрут
+def apply_custom_styles(m):
+    """Используется пользовательский CSS, чтобы сделать элемент управления слоем больше и заметнее
+    Примечание: не работает, если пытаться использовать в index.html файле. Необходимо добавлять через Python."""
+    custom_css = """
+    <style>
+        .leaflet-control-layers {
+            font-size: 18px; 
+            width: 250px; 
+            padding: 10px; 
+        }
+        .leaflet-control-layers label {
+            font-size: 18px;
+        }
+        .leaflet-control-layers-expanded {
+            background-color: rgba(255, 255, 255, 0.9);
+        }
+    </style>
+    """
+    m.get_root().html.add_child(folium.Element(custom_css))
+
+# Главный маршрут сервера (REST endpoint)
 @app.route('/')
 def index():
     """Отображает главную страницу с картой."""
